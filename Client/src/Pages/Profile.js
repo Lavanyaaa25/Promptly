b706bb5cf98d802e741d9d8e5f470b1962b64ced
Vaibhav Singh <vaibhavsingh12b.34@gmail.com>
2024-03-06 13:36:24 +0530

    Added user links

--- Client/src/Pages/Profile.js
+++ Client/src/Pages/Profile.js
@@ -4,9 +4,8 @@ import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListAlt, faPlus,faBookmark } from '@fortawesome/free-solid-svg-icons';
 import PromptCard from '../Components/Card2';
+import PromptCard2 from '../Components/Card1';
 import Photo from '../Assets/user-account.png';
-// import Header from '../Components/Header';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
 
 const UserProfile = () => {
@@ -16,8 +15,7 @@ const UserProfile = () => {
   const [prompts, setPrompts] = useState([]);
   const [saved, setSaved] = useState([]);
   const [shift, setShift] = useState(true);
-  // const [selectedOption, setSelectedOption] = useState('My Prompts');
-  // const likesCount = 25; // Replace with actual likes count
+  const [auth, setAuth] = useState(null);
   const { userName } = useParams();
   const navigate = useNavigate();
 
@@ -40,6 +38,7 @@ const UserProfile = () => {
             setPromptCount(data.prompts.length);
             setPrompts(data.prompts);
             setSaved(data.saved);
+            setAuth(data.auth);
             // console.log(prompts);
           }else{
             console.log("Hello");
@@ -67,8 +66,80 @@ const UserProfile = () => {
         <h1 className="ml-4 mt-8 font-semibold text-6xl">USER DOES NOT EXIST :(</h1>
       </div>
     );
-  else
-    return (
+  else{
+    if(auth){
+        return (
+            <div className="min-h-screen text-white relative flex flex-col gap-9">
+              {/* <Header/> */}
+              <div className="flex items-center justify-between">
+          <div className="ml-4  md:text-2xl font-bold text-sm text-purple flex items-center">
+            <span onClick={() => navigate('/explore')} className="hover:cursor-pointer">
+              Promptly
+            </span>
+          </div>
+            <button
+              className={`button-signout py-3 px-2 mr-4 rounded-md cursor-pointer transition-all duration-150 text-md text-pink font-bold`}
+              onClick={handleSignOut}
+            >
+              <FontAwesomeIcon icon={faSignOutAlt} className="mr-1 mt-2" />
+              Log Out
+            </button>
+        </div>
+
+            <div className="flex justify-center">
+              <div className="border border-white rounded-lg p-4 w-3/4">
+                <div className="flex items-center mb-6">
+                  <div className="rounded-full h-24 w-24 overflow-hidden border-2 border-white flex-shrink-0">
+                    <img className="h-full w-full object-cover" src={Photo} alt="Profile" />
+                  </div>
+
+                  <div className="ml-6 flex flex-col flex-grow">
+                    <div className="text-2xl font-semibold text-white mb-1">{username}</div>
+                    <div className="text-sm text-gray-300 flex items-center">
+                      <FontAwesomeIcon icon={faListAlt} className="mr-1 text-yellow-400" />
+                      <span className="text-yellow-400 text-lg mr-2">{promptCount}</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex justify-center">
+                  <button
+                    className={`button-create py-3 px-4 rounded-md cursor-pointer transition-all duration-150 text-lg text-black font-semibold bg-orange`}
+                    onClick={() => navigate('/create')}
+                  >
+                    <FontAwesomeIcon icon={faPlus} className="mr-2" />
+                    Create
+                  </button>
+                  <button
+                    className={`button-prompt py-3 px-4 rounded-md cursor-pointer transition-all duration-150 text-lg text-black font-semibold bg-green ml-4`}
+                    onClick={() => setShift(true)}
+                  >
+                    My Prompts
+                  </button>
+                  <button
+                    className={`button-saved py-3 px-4 rounded-md cursor-pointer transition-all duration-150 text-lg text-black font-semibold bg-green ml-4`}
+                    onClick={() => setShift(false)}
+                  >
+                    <FontAwesomeIcon icon={faBookmark} className="mr-2" />
+                    Saved
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row flex-wrap gap-4">
+              {
+                (shift)?
+                prompts.map((post) => (
+                <PromptCard type={shift} key={post.id} id={post.id} promptText={post.prompt} userName={post.username} />
+                )):saved.map((post) => (
+                <PromptCard type={shift} key={post.id} id={post.id} promptText={post.prompt} userName={post.username} />
+                ))
+              }
+            </div>
+          </div>
+        );
+    }else{
+      return (
         <div className="min-h-screen text-white relative flex flex-col gap-9">
           {/* <Header/> */}
           <div className="flex items-center justify-between">
@@ -98,48 +169,31 @@ const UserProfile = () => {
                 <div className="text-sm text-gray-300 flex items-center">
                   <FontAwesomeIcon icon={faListAlt} className="mr-1 text-yellow-400" />
                   <span className="text-yellow-400 text-lg mr-2">{promptCount}</span>
-                  {/* <FontAwesomeIcon icon={faHeart} className="mr-1 text-orange" />
-                  <span className="text-white text-lg">{likesCount}</span> */}
                 </div>
               </div>
             </div>
 
             <div className="flex justify-center">
               <button
-                className={`button-create py-3 px-4 rounded-md cursor-pointer transition-all duration-150 text-lg text-black font-semibold bg-orange`}
-                onClick={() => navigate('/create')}
-              >
-                <FontAwesomeIcon icon={faPlus} className="mr-2" />
-                Create
-              </button>
-              <button
                 className={`button-prompt py-3 px-4 rounded-md cursor-pointer transition-all duration-150 text-lg text-black font-semibold bg-green ml-4`}
                 onClick={() => setShift(true)}
               >
-                My Prompts
-              </button>
-              <button
-                className={`button-saved py-3 px-4 rounded-md cursor-pointer transition-all duration-150 text-lg text-black font-semibold bg-green ml-4`}
-                onClick={() => setShift(false)}
-              >
-                <FontAwesomeIcon icon={faBookmark} className="mr-2" />
-                Saved
+                Contributions
               </button>
             </div>
           </div>
         </div>
         <div className="flex flex-row flex-wrap gap-4">
           {
-            (shift)?
             prompts.map((post) => (
-            <PromptCard type={shift} key={post.id} id={post.id} promptText={post.prompt} userName={post.username} />
-            )):saved.map((post) => (
-            <PromptCard type={shift} key={post.id} id={post.id} promptText={post.prompt} userName={post.username} />
+            <PromptCard2 type={shift} key={post.id} id={post.id} promptText={post.prompt} userName={post.username} />
             ))
           }
         </div>
       </div>
     );
+    }
+  }
 };
 
 export default UserProfile;
--- Server/server.js
+++ Server/server.js
@@ -109,23 +109,42 @@ app.post('/users/profile',authenticateToken, async (req, res) => {
     try{
         const username = req.body.userName;
         const user = await User.findOne({username: username})
-        const prompts=[];
-        const saved=[];
-        for(var i=0;i<user.posts.length;i++){
-            const prompt = await Prompt.findOne({id: user.posts[i]});
-            if(prompt != null)
-            prompts.push(prompt);
+        if(!user){
+            return res.json({status: 'error'});
         }
-        for(var i=0;i<user.saved.length;i++){
-            const prompt = await Prompt.findOne({id: user.saved[i]});
-            if(prompt != null)
-            saved.push(prompt);
+        const token = req.headers['access-token'];
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const auth_user = await User.findOne({username: decoded.username});
+        if(auth_user.username === user.username){
+            const prompts=[];
+            const saved=[];
+            for(var i=0;i<user.posts.length;i++){
+                const prompt = await Prompt.findOne({id: user.posts[i]});
+                if(prompt != null)
+                prompts.push(prompt);
+            }
+            for(var i=0;i<user.saved.length;i++){
+                const prompt = await Prompt.findOne({id: user.saved[i]});
+                if(prompt != null)
+                saved.push(prompt);
+            }
+            // console.log(prompts);
+            if(user)
+                res.json({status: 'ok', user: user, prompts: prompts, saved: saved, auth: true});
+            else
+                res.json({status: 'error'});
+        }else{
+            const prompts=[];
+            for(var i=0;i<user.posts.length;i++){
+                const prompt = await Prompt.findOne({id: user.posts[i]});
+                if(prompt != null)
+                prompts.push(prompt);
+            }
+            if(user)
+                res.json({status: 'ok', user: user, prompts: prompts, auth: false});
+            else
+                res.json({status: 'error'});
         }
-        // console.log(prompts);
-        if(user)
-            res.json({status: 'ok', user: user, prompts: prompts, saved: saved});
-        else
-            res.json({status: 'error'});
     }catch(err){
         res.json({status: 'error', message: 'Invalid username'});
     }
